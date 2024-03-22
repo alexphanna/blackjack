53af8d3346b7a3040d52a54dee79090fe8df61b9
Alex Hanna <ahanna0608@gmail.com>
2024-03-22 15:20:15 -0400

    Add svg card

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 class Player {
-    constructor(name, hand = []) {
-        this.name = name;
-        this.hand = hand;
-        this.canvas = document.getElementById(name.toLowerCase() + "-canvas");
-        this.heading = document.getElementById(name.toLowerCase() + "-heading");
+    constructor(name) {
+        this.name = name 
+        this.hands = []
     }
+}
+class Hand {
+    constructor(cards = []) {
+        this.cards = cards;
+        this.canvas = document.getElementById(name.toLowerCase() + "-canvas");
+    }
+    
     get score() {
         var sum = [0];
         for (let card of this.hand) {
@@ -32,20 +37,20 @@ class Player {
         }
         return sum;
     }
-    drawHand() {
+    draw() {
         this.canvas.getContext("2d").clearRect(0, 0, this.canvas.width, this.canvas.height);
-        for (let i = 0; i < this.hand.length; i++) {
-            this.hand[i].draw(this.canvas.getContext("2d"), (200 - (this.hand.length - 1) * 15 - 25) + (i * 30), 15);
+        for (let i = 0; i < this.cards.length; i++) {
+            this.cards[i].draw(this.canvas.getContext("2d"), (200 - (this.cards.length - 1) * 15 - 25) + (i * 30), 15);
         }
     }
     hit() {
         let card = deck.pop();
-        if (this.name == "Dealer" && this.hand.length == 0) {
+        if (this.name == "Dealer" && this.cards.length == 0) {
             card.flip();
         }
-        this.hand.push(card);
+        this.cards.push(card);
         this.heading.innerHTML = `<b>${this.name}</b>: ${this.score}`;
-        this.drawHand();
+        this.drawCard();
         if (this.score.length == 0) {
             document.getElementById("buttons").style.display = "none";
             document.getElementById("restartButton").style.display = "inline";
@@ -60,8 +65,8 @@ class Player {
         }
     }
     stand() {
-        dealer.hand[0].flip();
-        dealer.drawHand();
+        dealer.cards[0].flip();
+        dealer.drawCard();
         dealer.heading.innerHTML = `<b>${dealer.name}</b>: ${dealer.score}`;
         // While dealer's highest score is less than 17, hit
         while (dealer.score[dealer.score.length - 1] < 17) {
@@ -85,8 +90,7 @@ class Player {
         }
     }
     double() {
-        this.hit();
-        this.stand();
+        // do nothing
     }
     split() {
         // do nothing
@@ -154,65 +158,26 @@ class Card {
                 return "♣";
         }
     }
-    // Draw the card on the canvas
-    draw(ctx, x, y) {
-        if (this.faceUp) {
-            ctx.fillStyle = "white";
-            ctx.fillRect(x, y, 50, 70);
-            ctx.strokeStyle = "black";
-            ctx.strokeRect(x, y, 50, 70);
-            ctx.font = "20px Arial, sans-serif";
-            if (this.suit == 1 || this.suit == 2) {
-                ctx.fillStyle = "red";
-            }
-            else {
-                ctx.fillStyle = "black";
-            }
-            ctx.fillText(this.rank, x + 5, y + 20);
-            ctx.fillText(this.symbol, x + 5, y + 40);
-        }
-        else {
-            ctx.fillStyle = "darkblue";
-            ctx.fillRect(x, y, 50, 70);
-            ctx.strokeStyle = "black";
-            ctx.strokeRect(x, y, 50, 70);
-        }
-    }
     // Flip the card
     flip() {
         this.faceUp = !this.faceUp;
     }
 }
 
-function start() {
-    deck = Deck.createDeck();
-    deck = Deck.shuffle(deck);  
-
-    // Create players
-    dealer = new Player("Dealer");
-    player = new Player("Player");
+function draw() {
+    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     
-    // Deal cards
-    dealer.hit();
-    dealer.hit();
-    player.hit();
-    player.hit();
+    var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    rect.setAttribute('width', '50');
+    rect.setAttribute('height', '70');
+    rect.setAttribute('x', '0');
+    rect.setAttribute('y', '20');
+    rect.setAttribute('rx', '5');
+    rect.setAttribute('ry', '5');
+    rect.setAttribute('fill', 'white');
+    svg.appendChild(rect);
 
-    heading.style.color = "black";
-    heading.innerHTML = "Blackjack";
-    document.getElementById("buttons").style.display = "inline";
-    document.getElementById("restartButton").style.display = "none";
-
-    if (player.score[player.score.length - 1] == 21) {
-        heading.style.color = "lime";
-        heading.innerHTML = "Blackjack";
-        document.getElementById("buttons").style.display = "none";
-        document.getElementById("restartButton").style.display = "inline";
-    }
+    document.body.appendChild(svg);
 }
 
-var deck;
-var dealer;
-var player;
-
-start();
+draw();
